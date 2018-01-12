e0f656b776c867998c6dfc55f8fff44c74168cd2
hwlu <haiwei.lu@99wuxian.com>
2018-01-12 17:22:23 +0800

    update

---
+++ dlnap/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name='dlnap',
+      version='0.14',
+      description='Python over the network media player to playback on DLNA UPnP devices',
+      author='cherezov',
+      author_email='cherezov.pavel@gmail.com',
+      url='https://github.com/cherezov/dlnap',
+      license='MIT',
+      platforms=['all'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: Jython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+      py_modules=['dlnap'],
+      install_requires=[
+          'xmltodict==0.11.0',
+      ],
+)
